26207dee2961fce16a0b4dc8e31f0e28e6e96976
tingyun <tingyun@pinduoduo.com>
2020-09-12 09:52:16 +0800

    refactor(root): update style

--- src/editor/LeftPannel/Apis/index.tsx
+++ src/editor/LeftPannel/Apis/index.tsx
@@ -1,9 +1,10 @@
 import React, { useCallback, useState, useRef, useEffect } from 'react';
-import { List, Button, Form, Input, Modal, Select } from 'antd';
+import { List, Button, Form, Input, Popover, Select } from 'antd';
 import { EditOutlined } from '@ant-design/icons';
 import isEqual from 'lodash/isEqual';
 import { observer } from 'mobx-react';
 import { Store } from '@/store';
+import { CodeIcon } from '.././../icons/Code';
 import styles from './index.module.scss';
 
 const { Option } = Select;
@@ -15,6 +16,7 @@ interface IProps {
 const methods = ['get', 'post', 'patch', 'delete'];
 
 export const Apis: React.FC<IProps> = observer(({ store }) => {
+  const ref = useRef<HTMLDivElement>();
   const apis = store.apiStore.apis;
   const names = apis.map((api) => api.name);
   const form = useRef(null);
@@ -47,38 +49,33 @@ export const Apis: React.FC<IProps> = observer(({ store }) => {
   }, [toEdited, toAdd]);
 
   return (
-    <div className={styles.wrapper}>
-      <Button block={true} onClick={() => setToAdd(true)}>
-        添加
-      </Button>
-      <List
-        size="small"
-        dataSource={apis}
-        renderItem={(item) => (
-          <List.Item
-            key={item.name}
-            actions={[
-              <EditOutlined
-                key="edit"
-                className={styles.editorWrapper}
-                onClick={() => setToEdited(item)}
-              />,
-            ]}
-          >
-            {item.name}
-          </List.Item>
-        )}
-      />
-      <Modal title={'接口'} visible={toAdd || Boolean(toEdited)} onCancel={close} footer={null}>
+    <Popover
+      title={null}
+      visible={toAdd || Boolean(toEdited)}
+      placement={'rightBottom'}
+      onVisibleChange={(v) => !v && close()}
+      trigger={'click'}
+      content={
         <Form
           name="basic"
           layout="vertical"
+          className={styles.formWrapper}
           ref={form}
           initialValues={toAdd ? null : toEdited}
           onFinish={onFinish}
+          style={{ width: 392, padding: 16 }}
         >
+          <h2>
+            <CodeIcon />
+            <span>接口</span>
+          </h2>
           <Form.Item
-            label="Name"
+            label={
+              <div className={styles.labelWrapper}>
+                <h3>接口名称</h3>
+                <p>为接口命名以便在页面中使用</p>
+              </div>
+            }
             name="name"
             rules={[
               { required: true, message: '请输入' },
@@ -96,10 +93,16 @@ export const Apis: React.FC<IProps> = observer(({ store }) => {
           >
             <Input />
           </Form.Item>
-          <Form.Item label="URL" name="url" rules={[{ required: true, message: '请输入' }]}>
-            <Input />
-          </Form.Item>
-          <Form.Item label="Method" name="method" rules={[{ required: true, message: '选择' }]}>
+          <Form.Item
+            label={
+              <div className={styles.labelWrapper}>
+                <h3>请求类型</h3>
+                <p>选择接口请求类型</p>
+              </div>
+            }
+            name="method"
+            rules={[{ required: true, message: '选择' }]}
+          >
             <Select>
               {methods.map((method) => (
                 <Option key={method} value={method}>
@@ -108,13 +111,59 @@ export const Apis: React.FC<IProps> = observer(({ store }) => {
               ))}
             </Select>
           </Form.Item>
+          <Form.Item
+            label={
+              <div className={styles.labelWrapper}>
+                <h3>接口地址</h3>
+                <p>输入接口服务地址</p>
+              </div>
+            }
+            name="url"
+            rules={[{ required: true, message: '请输入' }]}
+          >
+            <Input autoFocus={true} />
+          </Form.Item>
+
           <Form.Item>
             <Button type="primary" htmlType="submit">
-              提交
+              保存
             </Button>
           </Form.Item>
         </Form>
-      </Modal>
-    </div>
+      }
+    >
+      <div ref={ref} className={styles.wrapper}>
+        <Button
+          block={true}
+          onClick={() => {
+            setToAdd(true);
+            setToEdited(null);
+          }}
+        >
+          添加
+        </Button>
+        <List
+          size="small"
+          dataSource={apis}
+          renderItem={(item) => (
+            <List.Item
+              key={item.name}
+              actions={[
+                <EditOutlined
+                  key="edit"
+                  className={styles.editorWrapper}
+                  onClick={() => {
+                    setToAdd(false);
+                    setToEdited(item);
+                  }}
+                />,
+              ]}
+            >
+              {item.name}
+            </List.Item>
+          )}
+        />
+      </div>
+    </Popover>
   );
 });
---
+++ src/editor/icons/Code.tsx
@@ -0,0 +1,147 @@
+import React from 'react';
+
+export const CodeIcon = () => {
+  return (
+    <svg
+      viewBox="0 0 1024 1024"
+      version="1.1"
+      xmlns="http://www.w3.org/2000/svg"
+      width="1em"
+      height="1em"
+    >
+      <path d="M16 0v1024" fill="" p-id="6143"></path>
+      <path d="M32 0v1024" fill="" p-id="6144"></path>
+      <path d="M48 0v1024" fill="" p-id="6145"></path>
+      <path d="M64 0v1024" fill="" p-id="6146"></path>
+      <path d="M80 0v1024" fill="" p-id="6147"></path>
+      <path d="M96 0v1024" fill="" p-id="6148"></path>
+      <path d="M112 0v1024" fill="" p-id="6149"></path>
+      <path d="M128 0v1024" fill="" p-id="6150"></path>
+      <path d="M144 0v1024" fill="" p-id="6151"></path>
+      <path d="M160 0v1024" fill="" p-id="6152"></path>
+      <path d="M176 0v1024" fill="" p-id="6153"></path>
+      <path d="M192 0v1024" fill="" p-id="6154"></path>
+      <path d="M208 0v1024" fill="" p-id="6155"></path>
+      <path d="M224 0v1024" fill="" p-id="6156"></path>
+      <path d="M240 0v1024" fill="" p-id="6157"></path>
+      <path d="M256 0v1024" fill="" p-id="6158"></path>
+      <path d="M272 0v1024" fill="" p-id="6159"></path>
+      <path d="M288 0v1024" fill="" p-id="6160"></path>
+      <path d="M304 0v1024" fill="" p-id="6161"></path>
+      <path d="M320 0v1024" fill="" p-id="6162"></path>
+      <path d="M336 0v1024" fill="" p-id="6163"></path>
+      <path d="M352 0v1024" fill="" p-id="6164"></path>
+      <path d="M368 0v1024" fill="" p-id="6165"></path>
+      <path d="M384 0v1024" fill="" p-id="6166"></path>
+      <path d="M400 0v1024" fill="" p-id="6167"></path>
+      <path d="M416 0v1024" fill="" p-id="6168"></path>
+      <path d="M432 0v1024" fill="" p-id="6169"></path>
+      <path d="M448 0v1024" fill="" p-id="6170"></path>
+      <path d="M464 0v1024" fill="" p-id="6171"></path>
+      <path d="M480 0v1024" fill="" p-id="6172"></path>
+      <path d="M496 0v1024" fill="" p-id="6173"></path>
+      <path d="M512 0v1024" fill="" p-id="6174"></path>
+      <path d="M528 0v1024" fill="" p-id="6175"></path>
+      <path d="M544 0v1024" fill="" p-id="6176"></path>
+      <path d="M560 0v1024" fill="" p-id="6177"></path>
+      <path d="M576 0v1024" fill="" p-id="6178"></path>
+      <path d="M592 0v1024" fill="" p-id="6179"></path>
+      <path d="M608 0v1024" fill="" p-id="6180"></path>
+      <path d="M624 0v1024" fill="" p-id="6181"></path>
+      <path d="M640 0v1024" fill="" p-id="6182"></path>
+      <path d="M656 0v1024" fill="" p-id="6183"></path>
+      <path d="M672 0v1024" fill="" p-id="6184"></path>
+      <path d="M688 0v1024" fill="" p-id="6185"></path>
+      <path d="M704 0v1024" fill="" p-id="6186"></path>
+      <path d="M720 0v1024" fill="" p-id="6187"></path>
+      <path d="M736 0v1024" fill="" p-id="6188"></path>
+      <path d="M752 0v1024" fill="" p-id="6189"></path>
+      <path d="M768 0v1024" fill="" p-id="6190"></path>
+      <path d="M784 0v1024" fill="" p-id="6191"></path>
+      <path d="M800 0v1024" fill="" p-id="6192"></path>
+      <path d="M816 0v1024" fill="" p-id="6193"></path>
+      <path d="M832 0v1024" fill="" p-id="6194"></path>
+      <path d="M848 0v1024" fill="" p-id="6195"></path>
+      <path d="M864 0v1024" fill="" p-id="6196"></path>
+      <path d="M880 0v1024" fill="" p-id="6197"></path>
+      <path d="M896 0v1024" fill="" p-id="6198"></path>
+      <path d="M912 0v1024" fill="" p-id="6199"></path>
+      <path d="M928 0v1024" fill="" p-id="6200"></path>
+      <path d="M944 0v1024" fill="" p-id="6201"></path>
+      <path d="M960 0v1024" fill="" p-id="6202"></path>
+      <path d="M976 0v1024" fill="" p-id="6203"></path>
+      <path d="M992 0v1024" fill="" p-id="6204"></path>
+      <path d="M1008 0v1024M0 16h1024" fill="" p-id="6205"></path>
+      <path d="M0 32h1024" fill="" p-id="6206"></path>
+      <path d="M0 48h1024" fill="" p-id="6207"></path>
+      <path d="M0 64h1024" fill="" p-id="6208"></path>
+      <path d="M0 80h1024" fill="" p-id="6209"></path>
+      <path d="M0 96h1024" fill="" p-id="6210"></path>
+      <path d="M0 112h1024" fill="" p-id="6211"></path>
+      <path d="M0 128h1024" fill="" p-id="6212"></path>
+      <path d="M0 144h1024" fill="" p-id="6213"></path>
+      <path d="M0 160h1024" fill="" p-id="6214"></path>
+      <path d="M0 176h1024" fill="" p-id="6215"></path>
+      <path d="M0 192h1024" fill="" p-id="6216"></path>
+      <path d="M0 208h1024" fill="" p-id="6217"></path>
+      <path d="M0 224h1024" fill="" p-id="6218"></path>
+      <path d="M0 240h1024" fill="" p-id="6219"></path>
+      <path d="M0 256h1024" fill="" p-id="6220"></path>
+      <path d="M0 272h1024" fill="" p-id="6221"></path>
+      <path d="M0 288h1024" fill="" p-id="6222"></path>
+      <path d="M0 304h1024" fill="" p-id="6223"></path>
+      <path d="M0 320h1024" fill="" p-id="6224"></path>
+      <path d="M0 336h1024" fill="" p-id="6225"></path>
+      <path d="M0 352h1024" fill="" p-id="6226"></path>
+      <path d="M0 368h1024" fill="" p-id="6227"></path>
+      <path d="M0 384h1024" fill="" p-id="6228"></path>
+      <path d="M0 400h1024" fill="" p-id="6229"></path>
+      <path d="M0 416h1024" fill="" p-id="6230"></path>
+      <path d="M0 432h1024" fill="" p-id="6231"></path>
+      <path d="M0 448h1024" fill="" p-id="6232"></path>
+      <path d="M0 464h1024" fill="" p-id="6233"></path>
+      <path d="M0 480h1024" fill="" p-id="6234"></path>
+      <path d="M0 496h1024" fill="" p-id="6235"></path>
+      <path d="M0 512h1024" fill="" p-id="6236"></path>
+      <path d="M0 528h1024" fill="" p-id="6237"></path>
+      <path d="M0 544h1024" fill="" p-id="6238"></path>
+      <path d="M0 560h1024" fill="" p-id="6239"></path>
+      <path d="M0 576h1024" fill="" p-id="6240"></path>
+      <path d="M0 592h1024" fill="" p-id="6241"></path>
+      <path d="M0 608h1024" fill="" p-id="6242"></path>
+      <path d="M0 624h1024" fill="" p-id="6243"></path>
+      <path d="M0 640h1024" fill="" p-id="6244"></path>
+      <path d="M0 656h1024" fill="" p-id="6245"></path>
+      <path d="M0 672h1024" fill="" p-id="6246"></path>
+      <path d="M0 688h1024" fill="" p-id="6247"></path>
+      <path d="M0 704h1024" fill="" p-id="6248"></path>
+      <path d="M0 720h1024" fill="" p-id="6249"></path>
+      <path d="M0 736h1024" fill="" p-id="6250"></path>
+      <path d="M0 752h1024" fill="" p-id="6251"></path>
+      <path d="M0 768h1024" fill="" p-id="6252"></path>
+      <path d="M0 784h1024" fill="" p-id="6253"></path>
+      <path d="M0 800h1024" fill="" p-id="6254"></path>
+      <path d="M0 816h1024" fill="" p-id="6255"></path>
+      <path d="M0 832h1024" fill="" p-id="6256"></path>
+      <path d="M0 848h1024" fill="" p-id="6257"></path>
+      <path d="M0 864h1024" fill="" p-id="6258"></path>
+      <path d="M0 880h1024" fill="" p-id="6259"></path>
+      <path d="M0 896h1024" fill="" p-id="6260"></path>
+      <path d="M0 912h1024" fill="" p-id="6261"></path>
+      <path d="M0 928h1024" fill="" p-id="6262"></path>
+      <path d="M0 944h1024" fill="" p-id="6263"></path>
+      <path d="M0 960h1024" fill="" p-id="6264"></path>
+      <path d="M0 976h1024" fill="" p-id="6265"></path>
+      <path d="M0 992h1024" fill="" p-id="6266"></path>
+      <path d="M0 1008h1024" fill="" p-id="6267"></path>
+      <path
+        d="M1024 938.667c0 47.147-38.187 85.333-85.333 85.333H85.334C38.187 1024 0.001 985.813 0.001 938.667V85.334C0.001 38.187 38.188 0.001 85.334 0.001h853.333C985.814 0.001 1024 38.188 1024 85.334v853.333z"
+        fill="currentColor"
+      ></path>
+      <path
+        d="M147.625 515.145v64l294.5 112v-69.5l-210.25-74.5 210.25-74.25v-69.75l-294.5 112z m408-212.25h59.25l-143.25 379.75h-58.75l142.75-379.75z m327.75 276.25l-294.5 112v-69.5l210.5-74.5-210.5-74.25v-69.75l294.5 112.5v63.5z"
+        fill="#FFFFFF"
+      ></path>
+    </svg>
+  );
+};
